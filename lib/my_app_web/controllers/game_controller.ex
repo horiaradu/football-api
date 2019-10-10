74f9214aaf78e0773c85614f1797c609403683d4
Horia Radu <horia.radu23@gmail.com>
2019-10-10 17:58:02 +0300

    add more swager + upcase season

--- lib/my_app_web/controllers/game_controller.ex
+++ lib/my_app_web/controllers/game_controller.ex
@@ -3,13 +3,23 @@ defmodule MyAppWeb.GameController do
   use PhoenixSwagger
 
   swagger_path :index do
-    get "/"
-    summary "List all games"
-    description "List all games"
-    response 200, "Ok", Schema.ref(:Games)
+    get("/api/games")
+    summary("List all games")
+    description("List all games")
+
+    parameters do
+      league(:query, :string, "The league for which to retrieve games")
+      season(:query, :string, "The season for which to retrieve games")
+    end
+
+    response(200, "Ok", Schema.ref(:Games))
   end
+
   def index(conn, params) do
-    qLeague = Map.get(params, "league")
+    qLeague =
+      Map.get(params, "league")
+      |> String.upcase()
+
     qSeason = Map.get(params, "season")
 
     data =
@@ -59,36 +69,40 @@ defmodule MyAppWeb.GameController do
 
   def swagger_definitions do
     %{
-      Game: swagger_schema do
-        title "Game"
-        description "A game"
-        properties do
-          id :string, "The ID of the game", required: true
-          league :string, "The name of the league", required: true
-          season :string, "The season", required: true
-          date :string, "The date of the game", format: "dd/MM/YYYY", required: true
-          home_team :string, "The name of the home team", required: true
-          away_team :string, "The name of the home team", required: true
-          fthg :string, "The number of goals scored by the home team", required: true
-          ftag :string, "The number of goals scored by the away team", required: true
+      Game:
+        swagger_schema do
+          title("Game")
+          description("A game")
+
+          properties do
+            id(:string, "The ID of the game", required: true)
+            league(:string, "The name of the league", required: true)
+            season(:string, "The season", required: true)
+            date(:string, "The date of the game", format: "dd/MM/YYYY", required: true)
+            home_team(:string, "The name of the home team", required: true)
+            away_team(:string, "The name of the home team", required: true)
+            fthg(:string, "The number of goals scored by the home team", required: true)
+            ftag(:string, "The number of goals scored by the away team", required: true)
+          end
+
+          example(%{
+            away_team: "Eibar",
+            date: "19/08/2016",
+            ftag: "1",
+            fthg: "2",
+            home_team: "La Coruna",
+            id: "1",
+            league: "SP1",
+            season: "201617"
+          })
+        end,
+      Games:
+        swagger_schema do
+          title("Games")
+          description("All games")
+          type(:array)
+          items(Schema.ref(:Game))
         end
-        example %{
-          away_team: "Eibar",
-          date: "19/08/2016",
-          ftag: "1",
-          fthg: "2",
-          home_team: "La Coruna",
-          id: "1",
-          league: "SP1",
-          season: "201617"
-      }
-      end,
-      Games: swagger_schema do
-        title "Games"
-        description "All games"
-        type :array
-        items Schema.ref(:Game)
-      end
     }
   end
 end
